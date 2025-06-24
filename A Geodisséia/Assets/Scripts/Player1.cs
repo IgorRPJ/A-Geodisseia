5d29ed9e3587ab0035d89efa130b506a5b7d59a5
juliabx <146197172+juliabx@users.noreply.github.com>
2025-06-24 15:38:23 -0300

    estou sentindo minhas forças indo embora

--- A Geodisséia/Assets/Scripts/Player1.cs
+++ A Geodisséia/Assets/Scripts/Player1.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Player1 : MonoBehaviour
 {
-        public float velocidade = 1;
+        public float velocidade = 5;
         Rigidbody2D rb;
         
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,27 +19,30 @@ public class Player1 : MonoBehaviour
 
     void Andar1()
     {
-       if (Input.GetKeyDown(KeyCode.D))
+       if (Input.GetKey(KeyCode.D))
        {
            Vector2 movement = new Vector2(velocidade, 0);
-           rb.AddForce(movement, ForceMode2D.Impulse);
+           //rb.AddForce(movement, ForceMode2D.Impulse);
+           rb.linearVelocity = new Vector2(velocidade, 0);
        }
        else if (Input.GetKey(KeyCode.A))
        {
            Vector2 movement = new Vector2(-velocidade, 0);
-           rb.AddForce(movement, ForceMode2D.Impulse);
+           //rb.AddForce(movement, ForceMode2D.Impulse);
+           rb.linearVelocity = new Vector2(-velocidade, 0);
        }
        
        else if (Input.GetKey(KeyCode.W))
        {
            Vector2 movement = new Vector2(0, velocidade);
-           rb.AddForce(movement, ForceMode2D.Impulse);
+           //rb.AddForce(movement, ForceMode2D.Impulse);
+           rb.linearVelocity = new Vector2(0, velocidade);
        }
        
-       else if (Input.GetKey(KeyCode.S))
+       else if (Input.GetKeyDown(KeyCode.S))
        {
+           Debug.Log("você abaixou, clicou o botão S");
            Vector2 movement = new Vector2(-0, velocidade);
-           rb.AddForce(movement, ForceMode2D.Impulse);
        }
     }
 }
