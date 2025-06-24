06e3332930b71a55684a115ffa9ad146f899bad2
juliabx <146197172+juliabx@users.noreply.github.com>
2025-06-24 15:52:41 -0300

    até q foi

--- A Geodisséia/Assets/Scripts/Player2.cs
+++ A Geodisséia/Assets/Scripts/Player2.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
 
 public class Player2 : MonoBehaviour
-{
+{public float velocidade = 5;
+    Rigidbody2D rb;
+        
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Andar1();
+    }
+
+    void Andar1()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector2 movement = new Vector2(velocidade, 0);
+            //rb.AddForce(movement, ForceMode2D.Impulse);
+            rb.linearVelocity = new Vector2(velocidade, 0);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector2 movement = new Vector2(-velocidade, 0);
+            //rb.AddForce(movement, ForceMode2D.Impulse);
+            rb.linearVelocity = new Vector2(-velocidade, 0);
+        }
+       
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector2 movement = new Vector2(0, velocidade);
+            //rb.AddForce(movement, ForceMode2D.Impulse);
+            rb.linearVelocity = new Vector2(0, velocidade);
+        }
+       
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Quadrium, clicou o botão S");
+            Vector2 movement = new Vector2(-0, velocidade);
+        }
     }
 }
