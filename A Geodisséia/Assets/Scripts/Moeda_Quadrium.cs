3bf3c7c4513d1a0233fc137173cbacf4a6943df6
juliabx <146197172+juliabx@users.noreply.github.com>
2025-11-24 18:06:36 -0300

    moeda fase 2

--- A Geodisséia/Assets/Scripts/Moeda_Quadrium.cs
+++ A Geodisséia/Assets/Scripts/Moeda_Quadrium.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Moeda_Quadrium : MonoBehaviour
 {
+    public GameObject somMoedaPrefab;
     void OnTriggerEnter2D(Collider2D collider)
     {
         Player2 p2 = collider.GetComponent<Player2>();
@@ -9,7 +10,26 @@ public class Moeda_Quadrium : MonoBehaviour
         if (p2 != null)
         {
             p2.ColetarMoeda();
+            Instantiate(somMoedaPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
 }
+/*using UnityEngine;
+
+public class Moeda_Quadrium : MonoBehaviour
+{
+     private AudioSource somMoeda;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Player2 p2 = collider.GetComponent<Player2>();
+
+        if (p2 != null)
+        {
+            p2.ColetarMoeda();
+            somMoeda.Play();
+            Destroy(gameObject);
+        }
+    }
+}*/
--- A Geodisséia/Assets/Scripts/Moeda_Trina.cs
+++ A Geodisséia/Assets/Scripts/Moeda_Trina.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Moeda_Trina : MonoBehaviour
 {
+    public GameObject somMoedaPrefab;
+
     void OnTriggerEnter2D(Collider2D collider)
     {
         Player1 p1 = collider.GetComponent<Player1>();
@@ -9,6 +11,7 @@ public class Moeda_Trina : MonoBehaviour
         if (p1 != null)
         {
             p1.ColetarMoeda();
+            Instantiate(somMoedaPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
