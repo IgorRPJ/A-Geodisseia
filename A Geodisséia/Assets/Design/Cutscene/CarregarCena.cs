c09ba3b02c9ea7e3fea6e1f12eff5d3599ad061b
juliabx <146197172+juliabx@users.noreply.github.com>
2025-11-24 20:22:29 -0300

    Cutscene Final, Final

--- A Geodisséia/Assets/Design/Cutscene/CarregarCena.cs
+++ A Geodisséia/Assets/Design/Cutscene/CarregarCena.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class CarregarCena : MonoBehaviour
 {
+    public string cenaParaCarregar;
+    public PlayableDirector director;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        director.stopped += OnTimelineFinished;
     }
 
     // Update is called once per frame
-    void Update()
+    void OnTimelineFinished(PlayableDirector obj)
     {
-        
+        SceneManager.LoadScene(cenaParaCarregar);
     }
 }
